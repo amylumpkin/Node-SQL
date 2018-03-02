9b673681603acbf198f60b18f35558e001808a6b
Amy <amysuelumpkin@gmail.com>
2018-03-02 12:58:34 -0500

    added js, not quite there

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ const mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: " ",
+    user: "root",
     password: " ",
     database: "bamazon_db"
 });
@@ -18,55 +18,46 @@ connection.connect(function(err) {
 });
 
 //quieres to get the data and then,
-//display the data
+//display the data, calling function to ask questions
 function start(){
     connection.query("SELECT * FROM products", function(err,results){
         if (err) throw err;
-        console.log(results);  //returns an array of objects
+        //console.log(results);  //returns an array of objects
+        console.log("\n--------- WELCOME TO BAMAZON -----------\n");
+        for (i = 0; i < results.length; i++){
+            console.log("ID: " + results[i].id + "   Product: " + results[i].product + "   Price: $" + results[i].price);
+        }
         questions();
     })
-}
+};
+
 //prompt users with 2 messages - with inquirer:
 // 1. Ask for ID of product they want
 // 2. Ask how many units they want
 function questions(){
+    connection.query("SELECT * FROM products", function(err, results){
+
     inquirer
     .prompt([  //prompt method returns a promise, kind of like a callback. 
         //have to have at least name and message.
         {
             name: "id",
-            type: "input", //input is the default, but leaving here so I'll know for future reference
+            type: "input", //input is the default type
             message: "What is the ID number of the product you want to buy?"
         },
         {
-            name: "units",
+            name: "quantity",
             type: "input",
             message: "How many units do you want to buy?"
-        },
-     ])
-     .then(function(answer){
-         console.log(answer);  //returns an object...ex) { id: '3', units: '20 }
-         //if they want more than what we have in stock, return 'insufficient quantity'
-         // call notEnough()
+        }
+    
+    ])
+    })
+};
+   // .then(function(answer){
+        //if answer is a valid id number, continue to q 2)
 
-         // else, call totalPrice()
-     })
-    };
+        // if quantitly is valid number, return product name, how many they want to order and the total price
 
-
-//functions for responses:
-//once order is placed app checks if store has enough of that product to meet customer's request
-// - if not, log 'insufficient quantity'
-function notEnough(){
-
-}
-// - if it does, fulfill the order, give the total price
-function totalPrice(){
-
-}
-
-
-
-// to fullfill the order, means to:
-// 1. update sql database to reflect remaining quantity
-// 2. after the update, show user their total cost of purchase
+        
+        //else 'not a valid id number'

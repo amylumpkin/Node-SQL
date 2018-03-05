07988536a516e3d381ff27044d07b7fa81e9d234
Amy <amysuelumpkin@gmail.com>
2018-03-05 18:12:03 -0500

    details

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,8 +6,8 @@ const mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "amy",
-    password: "Password",
+    user: "",
+    password: "",
     database: "bamazon_db"
 });
 
